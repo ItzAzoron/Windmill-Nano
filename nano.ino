bbc8ec81479fc53d6cbb164543e77f7dcb62ddc8
Lorenz Clijnen <lorenz_reclame@outlook.be>
2019-09-21 15:13:36 +0200

    encoder added

--- nano.ino
+++ nano.ino
@@ -3,22 +3,23 @@
 /*  Libaries */
 #include <Stepper.h>
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-#include <ClickEncoder.h>
-#include <TimerOne.h>
+#include "LiquidCrystal_I2C.h"
+#include "ClickEncoder.h"
+#include "TimerOne.h"
 
 /* Settings */
 const int stepsPerRevolution = 255; // Steps per revolution
 const int dirMoterSpeed = 100;      // Speed of stepper motor that turns the turbine
 const int R1 = 150;                 // Value of resistor 1 from the voltage divider
 const int R2 = 680;                 // Value of resistor 2 from the voltage divider
+const int pulsesPerDegree = 0;      // 
 
 /* Pin numbers */
-const int vanePot = A0;           // Analog input pin that the potentiometer is attached to
-const int windSpeedHall = 5;      // Digital input pin for hall sensor that reads wind speed
-const int voltageSensor = A2;     // Analog input pin at wich the voltage divider is conected
-const int currentSensor = A1;     // Analog sensor to mesuare the current
-
+const int vanePot = A0;               // Analog input pin that the potentiometer is attached to
+const int windSpeedHall = 5;          // Digital input pin for hall sensor that reads wind speed
+const int voltageSensor = A2;         // Analog input pin at wich the voltage divider is conected
+const int currentSensor = A1;         // Analog sensor to mesuare the current
+const int encoderPins[] = {A3,A4,A5}; // Pins of the encoder, A5 is button
 
 /* initialize variables */
 int vaneValue = 0;
@@ -27,12 +28,15 @@ int currentLcdMenu = 0;
 bool isInMaintenance = false;
 Stepper dirMotor(stepsPerRevolution, 8, 10, 9, 11); // Initialize the stepper for wind direction control:
 LiquidCrystal_I2C lcd(0x27,20,4);                   // Initialize the lcd on i2c adress 0x27
+ClickEncoder *encoder;                              // Create encoder variable
 
 void setup() {
-  #ifdef debug
+  #ifdef DEBUG
     Serial.begin(9600);   // Initialize serial at baudrate 9600
+    Serial.println("booting...");
   #endif
   dirMotor.setSpeed(dirMoterSpeed);  // Sets the speed at wich the turbine will rotate
+  encoder = new ClickEncoder(encoderPins[0], encoderPins[1], encoderPins[2]);
   attachInterrupt(digitalPinToInterrupt(windSpeedHall), setWindSpeed, HIGH);
   lcd.init();
   lcd.backlight();
@@ -40,17 +44,47 @@ void setup() {
   lcd.print("Welcome");
   lcd.setCursor(1,1);
   lcd.print("Loading...");
+  Timer1.initialize(1000);
+  Timer1.attachInterrupt(timerIsr);
 }
 
 void loop() {
+  setTurbineDirection(getWindDirection());
+  handleEncoder();
   delay(2);
   
+}
+
+void timerIsr() {
+  encoder->service();
+}
+
+void handleEncoder() {
+
+  #ifdef DEBUG
+    Serial.print("encoder val: ");
+    Serial.println(encoder->getValue());
+  #endif
+
+  ClickEncoder::Button encoderButton = encoder->getButton();  // Reads the status of the button
+
+  if(encoderButton != ClickEncoder::Open) {   // Check if the button is pressed
+    switch (encoderButton)
+    {
+      case ClickEncoder::Clicked :
+        #ifdef DEBUG
+          Serial.println("clicked");
+        #endif
+
+        break;
+    }
+  }
 }
 
 /* Returns the direction of the wind in degrees relative to the turbine rotation */
 int getWindDirection(){
   int wDirection = 0;
-  //TODO read pot meter and convert into degrees rel to the turbine
+  wDirection= (511 - analogRead(vanePot))/pulsesPerDegree; // Convert analog reading to degrees  
   return wDirection;
 }
 
@@ -74,11 +108,17 @@ void setWindSpeed(){
 }
 
 /* Scroll through the options on lcd */
-void scollDisplay(){
+void scollDisplay(bool dir){
   int mainMenuItems = 5;
-  currentLcdMenu++;
+  if(dir)
+    currentLcdMenu++;
+  else
+    currentLcdMenu--;
+
   if(currentLcdMenu >= mainMenuItems)
     currentLcdMenu = 0;
+  else if(currentLcdMenu < 0)
+    currentLcdMenu = mainMenuItems-1;
    
   lcdMainMenu(currentLcdMenu);
     
